cc9d1dc41504d174d28f1f2f5d63caabe62bebfe
JJMiranda <jjmiranda@outlook.com>
2023-06-28 19:26:48 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Socket server for interactive demos.
+
+## Install
+* npm install
+* npm run dev
+
+### Running
+* Execute in port 8002
+* create a .env using example.env like a template.
+
+by DTI LAB - NTTDATA Perú
---
+++ index.ts
@@ -0,0 +1,59 @@
+import socket from "socket.io";
+import dotenv from 'dotenv';
+import logger from "./logger";
+
+dotenv.config();
+const port = +(process.env.PORT || "8002");
+
+class App {
+
+    public io: socket.Server;
+
+    constructor() {
+        this.io = new socket.Server(port, {cors: { origin: '*'}});
+        this.pipes();
+    }
+
+    private pipes(): void { 
+        logger.info(`Server is running at https://localhost:${port}`);
+        this.io.on("connection", (socket) => {
+            socket.on("shared-identity-provider", (args) => {
+                logger.info(`Emitiendo un shared-identity-provider-client`);
+                socket.to(args.to).emit('shared-identity-provider-client', args.content);
+            });
+            socket.on("shared-identity-provider-2", (args) => {
+                logger.info(`Emitiendo un shared-identity-provider-2-client`);
+                socket.to(args.to).emit('shared-identity-provider-2-client', args.content);
+            });
+            socket.on("shared-identity-ask", (args) => {
+                logger.info(`Emitiendo un shared-identity-ask-client`);
+                socket.to(args.to).emit('shared-identity-ask-client', args.content);
+            });
+            socket.on("shared-identity-custom", (args) => {
+                logger.info(`Emitiendo un shared-identity-custom-client`);
+                socket.to(args.to).emit('shared-identity-custom-client', args.content);
+            });
+            socket.on("shared-identity-custom-2", (args) => {
+                logger.info(`Emitiendo un shared-identity-custom-2-client`);
+                socket.to(args.to).emit('shared-identity-custom-2-client', args.content);
+            });
+            socket.on("shared-identity", (args) => {
+                logger.info(`Emitiendo un shared-identity-client`);
+                socket.to(args.to).emit('shared-identity-client', args.content);
+            });
+            socket.on("shared-credential", (args) => {
+                logger.info(`Emitiendo un shared-credential-client`);
+                socket.to(args.to).emit('shared-credential-client', args.content);
+            });
+            socket.on("vc-downloaded", (args) => {
+                logger.info(`Emitiendo un vc-downloaded-client`);
+                socket.to(args.to).emit('vc-downloaded-client', args.content);
+            });
+            socket.on("access-history", (args) => {
+                socket.broadcast.emit('access-history-client', args.content);
+            });
+        });
+    }
+}
+
+export default new App().io;
